8700ddc17d56996485ca4073e601efd23d1b1fbb
toky rasolofo <toky rasolofo>
2017-01-11 21:34:38 +0100

    Netoyage Classe Database

--- Database.php
+++ Database.php
@@ -1,37 +1,22 @@
 <?php 
 
-class Database 
+class Database
 {
-	
-	private static $database; //si on veut utiliser une variable dans une fonction static il faut que celle la soit aussi static
-	public $dbparams;
-	public $user;
-	private $pwd;
-
-	public function setDbparams($name, $hostName, $type)
-	{
-		$this->dbparams = 'mysql:host=' . $hostName . ';dbname=' . $name . ';charset=' . $type;
-	}
+	private static $connection = null;
+	private static $dbName = 'blog'; //si on veut utiliser une variable dans une fonction static il faut que celle la soit aussi static
+	private static $host = 'localhost';
+	private static $user = 'root';
+	private static $password = '';
 
 	public static function connect() //ici nous avons une fonction static qui pourra s'appeler sans l'instantiation d'une class
 	{
-		try
-		{
-			self::$database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8'/*$this->dbparams*/, 'root'/*$this->user*/, ''/*$this->pwd*/);
-			echo 'database ouvert';
-			return self::$database; //l'utilisation d'une variable static s'utilise avec self::nomAttribut
- 		}
-		catch(Exception $e)
-		{
-		        die('Erreur : '.$e->getMessage());
-		}
+		self::$connection = new PDO('mysql:host='.self::$host.';dbname='.self::$dbName.';charset=utf8', self::$user, self::$password);
+		return self::$connection; //l'utilisation d'une variable static s'utilise avec self::nomAttribut
 	}
-
 
 	public static function disconnect()
 	{
-		self::$database = NULL; //je remet database a NULL pour ne pas pourrir ma base avec d'autre elements
-		echo 'database fermé';
+		self::$connection = NULL; //je remet database a NULL pour ne pas pourrir ma base avec d'autre elements
 	}
 }
 
